8eb43ef7907117cadbbd772968aee2581e362954
oc <ckcz123@126.com>
2017-11-27 23:56:35 +0800

    Reformat code

--- libs/events.js
+++ libs/events.js
@@ -37,7 +37,8 @@ events.prototype.init = function () {
                 callback();
         },
         'passNet': function (data, core, callback) {
-            core.passNet(data.event.id);
+            // core.passNet(data.event.id);
+            this.passNet(data);
             if (core.isset(callback))
                 callback();
         }
@@ -52,3 +53,206 @@ events.prototype.getEvents = function (eventName) {
 }
 
 main.instance.events = new events();
+
+////// 选中菜单栏 //////
+events.prototype.selectSettings = function (y) {
+    if (y == 3) {
+        if (core.musicStatus.isIOS) {
+            core.drawTip("iOS设备不支持播放音乐");
+            return;
+        }
+        core.changeSoundStatus();
+        core.openSettings(false);
+    }
+    if (y == 4) core.selectShop();
+    if (y == 5) core.events.decreaseHard();
+    if (y == 6) {
+        core.showConfirmBox('你确定要清空所有存档吗？', function(){
+            core.closePanel();
+            localStorage.clear();
+            core.drawTip("操作成功");
+        }, function() {
+            core.openSettings(false);
+        });
+    }
+    if (y == 7) {
+        core.showConfirmBox("你确定要重新开始吗？", function () {
+            // core.drawTip("重新开始游戏");
+            core.closePanel();
+            core.restart();
+        }, function () {
+            core.openSettings(false);
+        });
+    }
+    if (y==8) {
+        core.drawAbout();
+    }
+    if (y == 9) core.closePanel();
+    return;
+}
+
+
+////// 降低难度 //////
+events.prototype.decreaseHard = function() {
+    if (core.status.hard == 0) {
+        core.drawTip("当前已是难度0，不能再降低难度了");
+        return;
+    }
+    core.showConfirmBox("本次操作可生命+" + (1100 - 100 * core.status.hard) + "，确定吗？", function () {
+        var add = 1100 - 100 * core.status.hard;
+        core.status.hero.hp += add;
+        core.status.hard--;
+        core.updateStatusBar();
+        core.closePanel();
+        core.drawTip("降低难度成功，生命+" + add);
+    }, function () {
+        core.openSettings(false);
+    });
+}
+
+////// 能否使用快捷商店 //////
+events.prototype.canUseQuickShop = function(index) {
+    if (core.status.floorId == 'MT20') return '当前不能使用快捷商店。';
+    if (core.status.hero.flags.passLava) return '由于你刚刚经过岩浆，此时不得使用快捷商店。\n切换楼层后恢复。';
+    return null;
+}
+
+////// 尝试使用道具 //////
+events.prototype.useItem = function(itemId) {
+    console.log("使用道具："+core.material.items[itemId].name);
+    core.closePanel(false);
+
+    if (itemId=='book') {
+        core.openBook(false);
+        return;
+    }
+    if (itemId=='fly') {
+        core.useFly(false);
+        return;
+    }
+
+    // TODO add other items
+    if (core.canUseItem(itemId)) core.useItem(itemId);
+    else core.drawTip("当前无法使用"+core.material.items[itemId].name);
+}
+
+/****** 打完怪物 ******/
+events.prototype.afterBattle = function(enemyId, callback) {
+    if (core.status.floorId == 'MT14' && !core.enemyExists(5,9) && !core.enemyExists(7,9)) {
+        core.openDoor("specialDoor", 6, 8, false);
+    }
+    if (core.status.floorId == 'MT20') {
+        // 检查封印
+        if (!core.status.hero.flags.seal20F) {
+            // 四个角都存在，四边都不存在
+            if (core.enemyExists(5,5) && core.enemyExists(5,7) && core.enemyExists(7,7) && core.enemyExists(7,5)
+                && !core.enemyExists(5,6) && !core.enemyExists(7,6) && !core.enemyExists(6,5) && !core.enemyExists(6,7)) {
+                // 触发封印
+                core.status.hero.flags.seal20F = true;
+                core.material.enemys.fairy.hp /= 10;
+                core.material.enemys.fairy.atk /= 10;
+                core.material.enemys.fairy.def /= 10;
+                core.updateFg();
+                core.drawText([
+                    {'content': '啊，我怎么被封印了！\n能量只剩下一成了！', 'id': 'fairy'}
+                ]);
+                // core.drawTip("触发仙子封印");
+            }
+        }
+    }
+}
+
+/****** 经过路障 ******/
+events.prototype.passNet = function (id) {
+    if (id=='lavaNet') {
+        core.status.hero.hp -= 100;
+        if (core.status.hero.hp<=0) {
+            core.status.hero.hp=0;
+            core.updateStatusBar();
+            core.events.lose();
+            return;
+        }
+        core.status.hero.flags.passLava = true;
+        core.updateStatusBar();
+        core.drawTip('经过熔岩，生命-100');
+    }
+}
+
+// NPC自定义操作
+events.prototype.npcCustomAction = function (npcData) {
+
+}
+
+// 当点击(x,y)位置后自定义操作
+events.prototype.npcCustomActionOnClick = function (npcData, x, y) {
+
+}
+
+// NPC自定义事件处理
+events.prototype.npcCustomEffect = function (effect, npc) {
+
+}
+
+// 存档事件前一刻的处理
+events.prototype.beforeSaveData = function(data) {
+
+}
+
+// 读档事件后，载入事件前，对数据的处理
+events.prototype.afterLoadData = function(data) {
+    // 重新封印仙子
+    if (core.status.hero.flags.seal20F) {
+        var fairy = core.material.enemys.fairy;
+        fairy.hp/=10;
+        fairy.atk/=10;
+        fairy.def/=10;
+    }
+}
+
+events.prototype.win = function() {
+    // 获胜
+    core.drawText('恭喜通关难度'+core.status.hard+'！你的分数是：'+core.status.hero.hp+"\n欢迎截图到发布帖下进行炫耀！\n\n再次感谢对本塔的支持！");
+}
+
+events.prototype.lose = function() {
+    // 失败
+    core.drawText('很不好意思，但是你死了。', function () {
+        core.restart();
+    });
+}
+
+
+// UI 部分
+
+events.prototype.drawUIAbout = function (left, top, width, height) {
+    var text_start = left + 24;
+
+    // 名称
+    core.canvas.ui.textAlign = "left";
+    core.fillText('ui', "异空间", text_start, top+35, "#FFD700", "bold 22px Verdana");
+    core.fillText('ui', "HTML5复刻版", text_start+100, top+37, "#DDDDDD", "bold 15px Verdana");
+    core.fillText('ui', "作者： 艾之葵", text_start, top + 80, "#FFFFFF", "bold 17px Verdana");
+    core.fillText('ui', "原作： ss433_2", text_start, top + 112, "#FFFFFF", "bold 17px Verdana");
+    core.fillText('ui', "制作工具： WebStorm", text_start, top + 144, "#FFFFFF", "bold 17px Verdana");
+    core.fillText('ui', "测试平台： Chrome/微信/iOS", text_start, top + 176, "#FFFFFF", "bold 17px Verdana");
+    core.fillText('ui', '特别鸣谢： ss433_2', text_start, top+208);
+    var len = core.canvas.ui.measureText('特别鸣谢： ').width;
+    core.fillText('ui', 'iEcho', text_start+len, top+240);
+    core.fillText('ui', '打Dota的喵', text_start+len, top+272);
+    core.fillText('ui', 'HTML5魔塔交流群：539113091', text_start, top+304);
+
+    core.fillText('ui', '<点击任意位置继续>', 230, 368, '#CCCCCC', '13px Verdana');
+}
+
+events.prototype.drawUISettings = function (left, top, width, height) {
+    core.canvas.ui.textAlign = "center";
+    core.fillText('ui', "音乐： " + (core.musicStatus.soundStatus ? "[ON]" : "[OFF]"), 208, top + 56, "#FFFFFF", "bold 17px Verdana");
+    core.fillText('ui', "快捷商店", 208, top + 88, "#FFFFFF", "bold 17px Verdana");
+    core.fillText('ui', "降低难度", 208, top + 120, "#FFFFFF", "bold 17px Verdana");
+    core.fillText('ui', "清空存档", 208, top + 152, "#FFFFFF", "bold 17px Verdana");
+    core.fillText('ui', "重新开始", 208, top + 184, "#FFFFFF", "bold 17px Verdana");
+    core.fillText('ui', "关于本塔", 208, top + 216, "#FFFFFF", "bold 17px Verdana");
+    core.fillText('ui', "返回游戏", 208, top + 248, "#FFFFFF", "bold 17px Verdana");
+}
+
+
--- libs/items.js
+++ libs/items.js
@@ -60,3 +60,215 @@ items.prototype.getItems = function (itemName) {
 }
 
 main.instance.items = new items();
+
+
+items.prototype.useItem = function (itemId) {
+    // 使用道具
+    if (!this.canUseItem(itemId)) return;
+    var itemCls = core.material.items[itemId].cls;
+
+    // 永久道具
+    if (itemCls == 'constants') {
+        if (itemId=='book') {
+            core.drawEnemyBook(1);
+        }
+        if (itemId=='fly') {
+            core.drawFly(core.status.hero.flyRange.indexOf(core.status.floorId));
+        }
+    }
+    // 消耗道具
+    if (itemCls == 'tools') {
+        core.status.hero.items[itemCls][itemId]--;
+        if (itemId == 'earthquake' || itemId == 'bomb' || itemId == 'pickaxe') {
+            // 地震卷轴/炸弹/破墙镐
+            core.removeBlockByIds(core.status.floorId, core.status.event.data);
+            core.drawMap(core.status.floorId, function () {
+                core.drawHero(core.getHeroLoc('direction'), core.getHeroLoc('x'), core.getHeroLoc('y'), 'stop');
+                core.updateFg();
+                core.drawTip(core.material.items[itemId].name + "使用成功");
+                if (itemId == 'bomb') {
+                    core.events.afterBattle();
+                }
+            });
+        }
+        if (itemId == 'centerFly') {
+            // 对称飞
+            core.clearMap('hero', 0, 0, 416, 416);
+            core.setHeroLoc('x', core.status.event.data.x);
+            core.setHeroLoc('y', core.status.event.data.y);
+            core.drawHero(core.getHeroLoc('direction'), core.getHeroLoc('x'), core.getHeroLoc('y'), 'stop');
+            core.drawTip(core.material.items[itemId].name + "使用成功");
+        }
+        if (itemId == 'upFly' || itemId == 'downFly') {
+            // 上楼器/下楼器
+            core.changeFloor(core.status.event.data.id, null, {'direction': core.status.hero.loc.direction, 'x': core.status.event.data.x, 'y': core.status.event.data.y}, function (){
+                core.drawTip(core.material.items[itemId].name + "使用成功");
+            });
+        }
+        if (core.status.hero.items[itemCls][itemId]==0)
+            delete core.status.hero.items[itemCls][itemId];
+    }
+}
+
+items.prototype.canUseItem = function (itemId) {
+    // 没有道具
+    if (!core.hasItem(itemId)) return false;
+
+    var itemCls = core.material.items[itemId].cls;
+
+    if (itemId == 'book') return true;
+    if (itemId == 'fly') return core.status.hero.flyRange.indexOf(core.status.floorId)>=0;
+    if (itemId == 'pickaxe') {
+        // 破墙镐
+        var ids = [];
+        for (var i in core.status.thisMap.blocks) {
+            var block = core.status.thisMap.blocks[i];
+            if (core.isset(block.event) && block.event.id == 'yellowWall' && Math.abs(block.x-core.status.hero.loc.x)+Math.abs(block.y-core.status.hero.loc.y)<=1) {
+                ids.push(i);
+            }
+        }
+        if (ids.length>0) {
+            core.status.event.data = ids;
+            return true;
+        }
+        return false;
+    }
+    if (itemId == 'bomb') {
+        // 炸弹
+        var ids = [];
+        for (var i in core.status.thisMap.blocks) {
+            var block = core.status.thisMap.blocks[i];
+            if (core.isset(block.event) && block.event.cls == 'enemys' && Math.abs(block.x-core.status.hero.loc.x)+Math.abs(block.y-core.status.hero.loc.y)<=1) {
+                var enemy = core.material.enemys[block.event.id];
+                if (core.isset(enemy.bomb) && !enemy.bomb) continue;
+                ids.push(i);
+            }
+        }
+        if (ids.length>0) {
+            core.status.event.data = ids;
+            return true;
+        }
+        return false;
+    }
+    if (itemId == 'earthquake') {
+        var ids = []
+        for (var i in core.status.thisMap.blocks) {
+            var block = core.status.thisMap.blocks[i];
+            if (core.isset(block.event) && (block.event.id == 'yellowWall' || block.event.id == 'blueWall' || block.event.id == 'whiteWall'))
+                ids.push(i);
+        }
+        if (ids.length>0) {
+            core.status.event.data = ids;
+            return true;
+        }
+        return false;
+    }
+    if (itemId == 'centerFly') {
+        // 中心对称
+        var toX = 12 - core.getHeroLoc('x'), toY = 12-core.getHeroLoc('y');
+        var blocks = core.status.thisMap.blocks;
+        for (var s = 0; s < blocks.length; s++) {
+            if (blocks[s].x == toX && blocks[s].y == toY) {
+                return false;
+            }
+        }
+        core.status.event.data = {'x': toX, 'y': toY};
+        return true;
+    }
+    if (itemId == 'upFly') {
+        // 上楼器
+        if (core.status.floorId == 'MT20' || core.status.floorId == 'MT21') // 禁用条件
+            return false;
+        var toId = null, found = false; // 上楼后的楼层ID
+        for (var id in core.status.maps) {
+            if (found) {toId = id; break;}
+            if (id == core.status.floorId) found=true;
+        }
+        if (!found) return false;
+        // 检查是否存在block（不是空地）
+        var toX = core.getHeroLoc('x'), toY = core.getHeroLoc('y');
+        var blocks = core.status.maps[toId].blocks;
+        for (var s = 0; s < blocks.length; s++) {
+            if (blocks[s].x == toX && blocks[s].y == toY) {
+                return false;
+            }
+        }
+        // 可以上楼，记录下位置信息，返回true
+        core.status.event.data = {'id': toId, 'x': toX, 'y': toY};
+        return true;
+    }
+    if (itemId == 'downFly') {
+        // 下楼器
+        if (core.status.floorId == 'MT0' || core.status.floorId == 'MT21')
+            return false;
+        var toId = null;
+        for (var id in core.status.maps) {
+            if (id == core.status.floorId) break;
+            toId = id;
+        }
+        if (toId == null) return false;
+        var toX = core.getHeroLoc('x'), toY = core.getHeroLoc('y');
+        var blocks = core.status.maps[toId].blocks;
+        for (var s = 0; s < blocks.length; s++) {
+            if (blocks[s].x == toX && blocks[s].y == toY) {
+                return false;
+            }
+        }
+        core.status.event.data = {'id': toId, 'x': toX, 'y': toY};
+        return true;
+    }
+    return false;
+}
+
+items.prototype.getItemEffect = function(itemId, itemNum) {
+    var itemCls = core.material.items[itemId].cls;
+    // 消耗品
+    if (itemCls === 'items') {
+        if (itemId === 'redJewel') core.status.hero.atk += 3;
+        if (itemId === 'blueJewel') core.status.hero.def += 3;
+        if (itemId === 'greenJewel') core.status.hero.mdef += 3;
+        // if (itemId == 'yellowJewel') core.status.hero.atk+=0;
+        if (itemId === 'redPotion') core.status.hero.hp += 200;
+        if (itemId === 'bluePotion') core.status.hero.hp += 500;
+        if (itemId === 'yellowPotion') core.status.hero.hp += 500;
+        if (itemId === 'greenPotion') core.status.hero.hp += 800;
+        if (itemId === 'sword1') core.status.hero.atk += 10;
+        if (itemId === 'sword2') core.status.hero.atk += 100;
+        if (itemId === 'sword5') core.status.hero.atk += 1000;
+        if (itemId === 'shield1') core.status.hero.def += 10;
+        if (itemId === 'shield2') core.status.hero.def += 100;
+        if (itemId === 'shield5') {
+            core.status.hero.def += 1000;
+            core.status.hero.flags.hasShield5 = true;
+        }
+        if (itemId === 'bigKey') {
+            core.status.hero.items.keys.yellowKey++;
+            core.status.hero.items.keys.blueKey++;
+            core.status.hero.items.keys.redKey++;
+        }
+        if (itemId == 'superPotion') core.status.hero.hp *= 2;
+    }
+    else {
+        core.addItem(itemId, itemNum);
+    }
+}
+
+items.prototype.getItemEffectTip = function(itemId) {
+    if (itemId === 'redJewel') return "，攻击+3";
+    if (itemId === 'blueJewel') return "，防御+3";
+    if (itemId === 'greenJewel') return "，魔防+3";
+    // if (itemId == 'yellowJewel') ;
+    if (itemId === 'redPotion') return "，生命+200";
+    if (itemId === 'bluePotion') return "，生命+500";
+    if (itemId === 'yellowPotion') return "，生命+500";
+    if (itemId === 'greenPotion') return "，生命+800";
+    if (itemId === 'sword1') return "，攻击+10";
+    if (itemId === 'sword2') return "，攻击+100";
+    if (itemId === 'sword5') return "，攻击+1000";
+    if (itemId === 'shield1') return "，防御+10";
+    if (itemId === 'shield2') return "，防御+100";
+    if (itemId === 'shield5') return "，防御+1000";
+    if (itemId === 'bigKey') return "，全钥匙+1";
+    if (itemId === 'superPotion') return "，生命值翻倍";
+    return "";
+}
